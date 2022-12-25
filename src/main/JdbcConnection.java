6ca797cc6edca3e3a9a0ad36b4fe983974a01533
NAYAN S. SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-12-25 21:39:01 +0530

    update salary using statement

--- src/main/JdbcConnection.java
+++ src/main/JdbcConnection.java
@@ -6,35 +6,104 @@ import java.util.Enumeration;
 public class JDBCConnection {
     public static void main(String[] args) {
 
-        String URL = "jdbc:mysql://localhost:3306/payroll_service";
+        String URL = "package com.bridgelabz;
+
+import java.sql.*;
+import java.util.Enumeration;
+
+public class JDBCConnection {
+    public static void main(String[] args) {
+
+        String URL = "package com.bridgelabz;
+
+import java.sql.*;
+import java.util.Enumeration;
+
+public class JDBCConnection {
+    public static void main(String[] args) {
+
+        String URL = "jdbc:mysql://localhost:3306/payroll_service_new?characterEncoding=utf8";
         String USER = "root";
-        String PASS = "Yasin@786";
+        String PASS = "12345";
 
         Connection connection;
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("Driver loaded");
+
         } catch (ClassNotFoundException e) {
             System.out.println("Cannot find the driver in ths classpath");
         }
-
-        listDrivers();
-
-        System.out.println("Connecting to Database" + URL);
+        
         try {
             connection = DriverManager.getConnection(URL, USER, PASS);
-            System.out.println("Connection is Successful" + connection);
+            Statement statement = connection.createStatement();
+            statement.execute("update employee_payroll set salary=160000 where name='Mark'");
+            ResultSet result = statement.executeQuery("select * from employee_payroll");
+            while (result.next()){
+                System.out.println(result.getInt("id")+" " +
+                        result.getString(2) +" "+
+                        result.getDouble(4)+" "+
+                        result.getDate(5));
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-        private static void listDrivers() {
-            Enumeration<Driver> driverList = DriverManager.getDrivers();
-            while (driverList.hasMoreElements()) {
-                Driver driverClass = driverList.nextElement();
-                System.out.println(" " + driverClass.getClass().getName());
-            }
+}";
+        String USER = "root";
+        String PASS = "12345";
 
+        Connection connection;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Cannot find the driver in ths classpath");
         }
+        
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASS);
+            Statement statement = connection.createStatement();
+            statement.execute("update employee_payroll set salary=160000 where name='Mark'");
+            ResultSet result = statement.executeQuery("select * from employee_payroll");
+            while (result.next()){
+                System.out.println(result.getInt("id")+" " +
+                        result.getString(2) +" "+
+                        result.getDouble(4)+" "+
+                        result.getDate(5));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}";
+        String USER = "root";
+        String PASS = "Y12345";
+
+        Connection connection;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Cannot find the driver in ths classpath");
+        }
+        
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASS);
+            Statement statement = connection.createStatement();
+            statement.execute("update employee_payroll set salary=160000 where name='Mark'");
+            ResultSet result = statement.executeQuery("select * from employee_payroll");
+            while (result.next()){
+                System.out.println(result.getInt("id")+" " +
+                        result.getString(2) +" "+
+                        result.getDouble(4)+" "+
+                        result.getDate(5));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
