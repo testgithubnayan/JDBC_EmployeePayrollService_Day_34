4012fe8b84589604eb50901f5a9c9f4e3cbd6959
NAYAN S. SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-12-25 21:52:41 +0530

    update salary using prepared statement

--- src/main/JdbcConnection.java
+++ src/main/JdbcConnection.java
@@ -5,24 +5,12 @@ import java.sql.*;
 public class JDBCConnection {
     public static void main(String[] args) {
 
-
-
         String URL = "package com.bridgelabz;
 
 import java.sql.*;
-import java.util.Enumeration;
 
 public class JDBCConnection {
     public static void main(String[] args) {
-
-        String URL = "package com.bridgelabz;
-
-import java.sql.*;
-import java.util.Enumeration;
-
-public class JDBCConnection {
-    public static void main(String[] args) {
-
 
         String URL = "jdbc:mysql://localhost:3306/payroll_service_new?characterEncoding=utf8";
         String USER = "root";
@@ -37,7 +25,6 @@ public class JDBCConnection {
             System.out.println("Cannot find the driver in ths classpath");
         }
         
-
         try {
             connection = DriverManager.getConnection(URL, USER, PASS);
             PreparedStatement preparedStatement = connection.prepareStatement("update employee_payroll set salary=? where name=? ");
@@ -45,12 +32,6 @@ public class JDBCConnection {
             preparedStatement.setString(2,"Mark");
             preparedStatement.execute();
             ResultSet result = preparedStatement.executeQuery("select * from employee_payroll");
-
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASS);
-            Statement statement = connection.createStatement();
-            statement.execute("update employee_payroll set salary=160000 where name='Mark'");
-            ResultSet result = statement.executeQuery("select * from employee_payroll");
             while (result.next()){
                 System.out.println(result.getInt("id")+" " +
                         result.getString(2) +" "+
@@ -74,13 +55,13 @@ public class JDBCConnection {
             System.out.println("Cannot find the driver in ths classpath");
         }
         
-
         try {
             connection = DriverManager.getConnection(URL, USER, PASS);
-            Statement statement = connection.createStatement();
-            statement.execute("update employee_payroll set salary=160000 where name='Mark'");
-            ResultSet result = statement.executeQuery("select * from employee_payroll");
-
+            PreparedStatement preparedStatement = connection.prepareStatement("update employee_payroll set salary=? where name=? ");
+            preparedStatement.setDouble(1,150000);
+            preparedStatement.setString(2,"Mark");
+            preparedStatement.execute();
+            ResultSet result = preparedStatement.executeQuery("select * from employee_payroll");
             while (result.next()){
                 System.out.println(result.getInt("id")+" " +
                         result.getString(2) +" "+
@@ -91,36 +72,4 @@ public class JDBCConnection {
             e.printStackTrace();
         }
     }
-
-
-}";
-        String USER = "root";
-        String PASS = "Y12345";
-
-        Connection connection;
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-        } catch (ClassNotFoundException e) {
-            System.out.println("Cannot find the driver in ths classpath");
-        }
-        
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASS);
-            Statement statement = connection.createStatement();
-            statement.execute("update employee_payroll set salary=160000 where name='Mark'");
-            ResultSet result = statement.executeQuery("select * from employee_payroll");
-            while (result.next()){
-                System.out.println(result.getInt("id")+" " +
-                        result.getString(2) +" "+
-                        result.getDouble(4)+" "+
-                        result.getDate(5));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-
 }
