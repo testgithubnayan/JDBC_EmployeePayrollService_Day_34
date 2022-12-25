8d9fe20232385a67b034cc4eba9dbfeeb5fbda60
NAYAN S. SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-12-25 21:33:56 +0530

    update salary using repared statement

--- src/main/JdbcConnection.java
+++ src/main/JdbcConnection.java
@@ -1,7 +1,6 @@
 package com.bridgelabz;
 
 import java.sql.*;
-import java.util.Enumeration;
 
 public class JDBCConnection {
     public static void main(String[] args) {
@@ -14,27 +13,26 @@ public class JDBCConnection {
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("Driver loaded");
+
         } catch (ClassNotFoundException e) {
             System.out.println("Cannot find the driver in ths classpath");
         }
-
-        listDrivers();
-
-        System.out.println("Connecting to Database" + URL);
+        
         try {
             connection = DriverManager.getConnection(URL, USER, PASS);
-            System.out.println("Connection is Successful" + connection);
+            PreparedStatement preparedStatement = connection.prepareStatement("update employee_payroll set salary=? where name=? ");
+            preparedStatement.setDouble(1,150000);
+            preparedStatement.setString(2,"Mark");
+            preparedStatement.execute();
+            ResultSet result = preparedStatement.executeQuery("select * from employee_payroll");
+            while (result.next()){
+                System.out.println(result.getInt("id")+" " +
+                        result.getString(2) +" "+
+                        result.getDouble(4)+" "+
+                        result.getDate(5));
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-        private static void listDrivers() {
-            Enumeration<Driver> driverList = DriverManager.getDrivers();
-            while (driverList.hasMoreElements()) {
-                Driver driverClass = driverList.nextElement();
-                System.out.println(" " + driverClass.getClass().getName());
-            }
-
-        }
 }
