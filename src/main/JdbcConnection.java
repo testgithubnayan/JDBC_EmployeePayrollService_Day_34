f2f1facf511cbbff995d137fcb94f4d76fbbc8a4
NAYAN S. SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-12-25 21:54:15 +0530

    update salary using statement

--- src/main/JdbcConnection.java
+++ src/main/JdbcConnection.java
@@ -6,7 +6,6 @@ import java.util.Enumeration;
 public class JDBCConnection {
     public static void main(String[] args) {
 
-
         String URL = "package com.bridgelabz;
 
 import java.sql.*;
@@ -14,15 +13,6 @@ import java.util.Enumeration;
 
 public class JDBCConnection {
     public static void main(String[] args) {
-
-        String URL = "package com.bridgelabz;
-
-import java.sql.*;
-import java.util.Enumeration;
-
-public class JDBCConnection {
-    public static void main(String[] args) {
-
 
         String URL = "jdbc:mysql://localhost:3306/payroll_service_new?characterEncoding=utf8";
         String USER = "root";
@@ -54,37 +44,7 @@ public class JDBCConnection {
     }
 }";
         String USER = "root";
-        String PASS = "12345";
-
-        Connection connection;
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-        } catch (ClassNotFoundException e) {
-            System.out.println("Cannot find the driver in ths classpath");
-        }
-        
-
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASS);
-            Statement statement = connection.createStatement();
-            statement.execute("update employee_payroll set salary=160000 where name='Mark'");
-            ResultSet result = statement.executeQuery("select * from employee_payroll");
-            while (result.next()){
-                System.out.println(result.getInt("id")+" " +
-                        result.getString(2) +" "+
-                        result.getDouble(4)+" "+
-                        result.getDate(5));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-}";
-        String USER = "root";
-        String PASS = "Y12345";
+        String PASS = "Yasin@786";
 
         Connection connection;
 
@@ -110,5 +70,4 @@ public class JDBCConnection {
             e.printStackTrace();
         }
     }
-
 }
