e84e1d487ead784812160f4db567a682b1b0ff25
NAYAN S. SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-12-25 21:56:14 +0530

    update salary using prepared statemement

--- src/main/JdbcConnection.java
+++ src/main/JdbcConnection.java
@@ -5,44 +5,7 @@ import java.sql.*;
 public class JDBCConnection {
     public static void main(String[] args) {
 
-        String URL = "package com.bridgelabz;
-
-import java.sql.*;
-
-public class JDBCConnection {
-    public static void main(String[] args) {
-
-        String URL = "jdbc:mysql://localhost:3306/payroll_service_new?characterEncoding=utf8";
-        String USER = "root";
-        String PASS = "12345";
-
-        Connection connection;
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-        } catch (ClassNotFoundException e) {
-            System.out.println("Cannot find the driver in ths classpath");
-        }
-        
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASS);
-            PreparedStatement preparedStatement = connection.prepareStatement("update employee_payroll set salary=? where name=? ");
-            preparedStatement.setDouble(1,150000);
-            preparedStatement.setString(2,"Mark");
-            preparedStatement.execute();
-            ResultSet result = preparedStatement.executeQuery("select * from employee_payroll");
-            while (result.next()){
-                System.out.println(result.getInt("id")+" " +
-                        result.getString(2) +" "+
-                        result.getDouble(4)+" "+
-                        result.getDate(5));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-}";
+        String URL = "jdbc:mysql://localhost:3306/payroll_service";
         String USER = "root";
         String PASS = "12345";
 
