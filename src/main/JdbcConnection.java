b979138fa2af72717e0bb51cb718f5cb4747db1e
NAYAN S. SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-12-25 21:40:06 +0530

    retriving the employees

--- src/main/JdbcConnection.java
+++ src/main/JdbcConnection.java
@@ -6,9 +6,9 @@ import java.util.Enumeration;
 public class JDBCConnection {
     public static void main(String[] args) {
 
-        String URL = "jdbc:mysql://localhost:3306/payroll_service";
+        String URL = "jdbc:mysql://localhost:3306/payroll_service_new?characterEncoding=utf8";
         String USER = "root";
-        String PASS = "Yasin@786";
+        String PASS = "12345";
 
         Connection connection;
 
